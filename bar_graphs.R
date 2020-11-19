9da5bc28e6e2350bf75eda63bf866151cc1928b1
matt-bracci <mmbracci@gmail.com>
2020-11-19 10:36:52 -0500

    Update bar_graphs.R

--- bar_graphs.R
+++ bar_graphs.R
@@ -1,8 +1,9 @@
-bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col, xgp, fgp){
+
+bar_graph <- function(dat, to_ag, type_of_ag, type_of_bar, sz, ylav, dgdwid, ag_col, xgp, fgp){
   #dat = data frame to use
   #to_ag = T or F (do we need to aggregate the data or is it already prepared for graph?)
   #type_of_ag = 'sum', 'avg', 'percent', 'count'
-  #to_dodge = T or F (are we dodging or not i.e graphing across more than 1 grp or not
+  #type_of_bar = 'single' or 'dodge' or 'stack' 
   #sz = size of the label
   #ylav = y limit value
   #dgdwid == dodge width 
@@ -12,10 +13,34 @@ bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col
   
   
   #need to ensure arguments are proper 
+  if(to_ag != T & to_ag != F){
+    statement <- print("Please enter the to_ag argument as T or F")
+    return(statement)
+  }
+  if(type_of_ag != "avg" & type_of_ag != "count" & type_of_ag != "sum" & type_of_ag != "percent"){
+    statement <- print("Please enter the type_of_ag argument as 'avg', 'count', 'sum' or 'percent'")
+    return(statement)
+  }
+  if(type_of_bar != "single" & type_of_bar != "dodge" & type_of_bar != "stack"){
+    statement <- print("Please enter the type_of_bar argument as 'single', 'dodge', or 'stack'")
+    return(statement)
+  }
+  if(!is.numeric(sz) | !is.numeric(ylav)){
+    statement <- print("sz and ylav arguments must be numeric. One or both of these is currently not")
+    return(statement)
+  }
+  if(type_of_bar != "single" & (is.na(dgdwid) | !is.numeric(dgdwid))){
+    statement <- print("When type_of_bar argument is 'dodge' or 'stack', then must provide dgdwid argument as numeric")
+    return(statement)
+  }
+  if(type_of_bar == "stack" & type_of_ag == "avg"){
+    statement <- warning("Are you sure you want to show averages in a stacked format?")
+  }
+  
   
   
   #obtaining the data we want to graph 
-  if(to_ag == T & to_dodge == T){  #when we need to aggregate and dodge 
+  if(to_ag == T & (type_of_bar == "dodge" | type_of_bar == "stack")){  #when we need to aggregate on groups 
     if(type_of_ag == "avg"){
       ag <- aggregate(dat[[ag_col]], list(dat[[xgp]], dat[[fgp]]), mean, na.rm = TRUE)
     } else if(type_of_ag == "sum"){
@@ -27,7 +52,7 @@ bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col
       ag <- merge(fullag, ag, by = "Group.1")
       ag$Percent <- ag$x/ag$tot 
     }
-  } else if(to_ag == T & to_dodge == F){ #when we need to aggregate and not dodge 
+  } else if(to_ag == T){ #when we need to aggregate on single group 
     if(type_of_ag == "avg"){
       ag <- aggregate(dat[[ag_col]], list(dat[[xgp]]), mean, na.rm = TRUE)
     } else if(type_of_ag == "sum"){
@@ -41,15 +66,25 @@ bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col
   }
   
   #load the graphing theme 
-  theme_to_use <- theme(axis.text.x = element_text(angle = 45, size = 12, vjust = 1, hjust = 1),
-                            axis.text.y = element_text(size = 12),
-                            panel.background = element_rect(fill = "white"),
-                            panel.grid.major = element_line(color = "grey90"),
-                            panel.grid.minor = element_line(color = "grey90"),
-                            legend.position = "bottom")
+  if(type_of_ag == "percent" & type_of_bar == "stack"){ #if this is the graph, we don't need any y axis 
+    theme_to_use <- theme(axis.text.x = element_text(angle = 45, size = 12, vjust = 1, hjust = 1),
+                          axis.text.y = element_blank(),
+                          panel.background = element_rect(fill = "white"),
+                          panel.grid.major = element_line(color = "grey90"),
+                          panel.grid.minor = element_line(color = "grey90"),
+                          legend.position = "bottom")
+  } else{
+    theme_to_use <- theme(axis.text.x = element_text(angle = 45, size = 12, vjust = 1, hjust = 1),
+                          axis.text.y = element_text(size = 12),
+                          panel.background = element_rect(fill = "white"),
+                          panel.grid.major = element_line(color = "grey90"),
+                          panel.grid.minor = element_line(color = "grey90"),
+                          legend.position = "bottom")
+  }
+  
   
   #create the graphs
-  if(to_dodge == T){ #if we're dodging 
+  if(type_of_bar == "dodge"){ #if we're dodging 
     if(is.numeric(ag$Group.2)){
       ag$Group.2 <- as.factor(ag$Group.2)
     } #if the second group is numeric, needs to be converted to factor to graph
@@ -85,7 +120,49 @@ bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col
         scale_y_continuous(labels = scales::percent, limits = c(0, max(ag$Percent) + ylav)) +
         theme_to_use
     }
-  } else{ #if we're not dodging 
+  } else if(type_of_bar == "stack"){
+      if(type_of_ag == "avg"){ #since this is an uncommon graph (stacked avgs) - create prompt/error handle this w/warning message
+        ag$Group.2 <- reorder(ag$Group.2, ag$x, mean) #must re order factors baed on its highest values
+        ag$Group.2 <- factor(ag$Group.2, levels=rev(levels(ag$Group.2))) #to get largest on bottom must reverse these levels
+        g <- ggplot(data = ag, aes(x = Group.1, y = x, label = round(x, digits = 1), fill = Group.2)) +
+          geom_bar(stat = "identity") +
+          geom_text(fontface = 'bold', colour = "white", size = sz, position = position_stack(vjust = 0.7)) + 
+          #ylim(0, max(ag$x) + ylav) +
+          labs(x = ag_col, title = paste("Average", ag_col, "by", xgp, "by", fgp), y = xgp, fill = fgp) +
+          scale_fill_brewer(palette = "Dark2") +
+          theme_to_use
+      } else if(type_of_ag == "sum"){
+        ag$Group.2 <- reorder(ag$Group.2, ag$x, sum) #must re order factors baed on its highest values
+        #ag$Group.2 <- factor(ag$Group.2, levels=rev(levels(ag$Group.2))) #to get largest on bottom must reverse these levels
+        g <- ggplot(data = ag, aes(x = Group.1, y = x, label = round(x, digits = 0), fill = Group.2)) +
+          geom_bar(stat = "identity") +
+          geom_text(fontface = 'bold', colour = "white", size = sz, position = position_stack(vjust = 0.7)) + 
+          #ylim(0, max(ag$x) + ylav) +
+          labs(x = ag_col, title = paste("Total", ag_col, "by", xgp, "by", fgp), y = xgp, fill = fgp) +
+          scale_fill_brewer(palette = "Dark2") +
+          theme_to_use
+      } else if(type_of_ag == "count"){
+        ag$Group.2 <- reorder(ag$Group.2, ag$x, length) #must re order factors baed on its highest values
+        ag$Group.2 <- factor(ag$Group.2, levels=rev(levels(ag$Group.2))) #to get largest on bottom must reverse these levels
+        g <- ggplot(data = ag, aes(x = Group.1, y = x, label = round(x, digits = 0), fill = Group.2)) +
+          geom_bar(stat = "identity") +
+          geom_text(fontface = 'bold', colour = "white", size = sz, position = position_stack(vjust = 0.7)) + 
+          #ylim(0, max(ag$x) + ylav) +
+          labs(x = ag_col, title = paste("Count", ag_col, "by", xgp, "by", fgp), y = xgp, fill = fgp) +
+          scale_fill_brewer(palette = "Dark2") +
+          theme_to_use
+      } else{
+        ag$Group.2 <- reorder(ag$Group.2, ag$x, length) #must re order factors baed on its highest values
+        ag$Group.2 <- factor(ag$Group.2, levels=rev(levels(ag$Group.2))) #to get largest on bottom must reverse these levels
+        g <- ggplot(data = ag, aes(x = Group.1, y = x, label = paste0(round(100 * Percent, digits = 0),"%"), fill = Group.2)) +
+          geom_bar(stat = "identity") +
+          geom_text(fontface = 'bold', colour = "white", size = sz, position = position_stack(vjust = 0.7)) + 
+          #scale_y_continuous(labels = scales::percent) +
+          labs(x = ag_col, title = paste("Percent of", ag_col, "by", xgp, "by", fgp), y = xgp, fill = fgp) +
+          scale_fill_brewer(palette = "Dark2") +
+          theme_to_use
+      }
+  } else{ #if we just have a single group 
     if(type_of_ag == "avg"){
       g <- ggplot(data = ag, aes(x = Group.1, y = x)) + ylim(0, max(ag$x) + ylav) +
         geom_bar(stat = "identity", fill = rgb(32,55,100, maxColorValue = 255)) + 
@@ -117,53 +194,29 @@ bar_graph <- function(dat, to_ag, type_of_ag, to_dodge, sz, ylav, dgdwid, ag_col
 }
 
 
-#examples
-data("mtcars")
-bar_graph(dat = mtcars, to_ag = T, type_of_ag = "avg", to_dodge = F,
+
+#single bar pre aggregated
+avg <- aggregate(mtcars$mpg, list(mtcars$cyl), mean)
+bar_graph(dat = avg, to_ag = F, type_of_ag = "avg", type_of_bar = "single",
+          sz = 4, ylav = 3, dgdwid = NA,
+          ag_col = "mpg", xgp = "cyl") #need to adjust column names. still need to type names in. prob can be fixed together
+
+#single bar graph non-aggregated
+bar_graph(dat = mtcars, to_ag = T, type_of_ag = "Percent", type_of_bar = "single",
+          sz = 4, ylav = 0.05, dgdwid = NA,
+          ag_col = "mpg", xgp = "cyl")
+
+#dodged bar graph non-aggregated
+bar_graph(dat = mtcars, to_ag = T, type_of_ag = "sum", type_of_bar = "dodge",
           sz = 4, ylav = 3, dgdwid = 0.75, 
-          ag_col = "mpg", xgp = "cyl") #single bar graph of average non-aggregated
-
-bar_graph(dat = mtcars, to_ag = T, type_of_ag = "avg", to_dodge = T,
-          sz = 4, ylav = 3, dgdwid = 0.75, 
-          ag_col = "mpg", xgp = "cyl", fgp = "gear") #dodged bar graph of average non-aggregated
+          ag_col = "mpg", xgp = "cyl", fgp = "gear")
 
 
+#stacked bar graph non-aggregated
 mtcars$cat <- ifelse(mtcars$mpg < 20, "Under 20 mpg", "20+ Mpg")
 mtcars$ID <- 1:nrow(mtcars)
-
-bar_graph(dat = mtcars, to_ag = T, type_of_ag = "percent", to_dodge = F,
-          sz = 4, ylav = 0.03, dgdwid = 0.75, 
-          ag_col = "ID", xgp = "cat") #single percent non-aggregated
-
-bar_graph(dat = mtcars, to_ag = T, type_of_ag = "percent", to_dodge = T,
+bar_graph(dat = mtcars, to_ag = T, type_of_ag = "count", type_of_bar = "stack",
           sz = 4, ylav = 0.1, dgdwid = 0.75, 
-          ag_col = "ID", xgp = "cat", fgp = "cyl") #dodged percent non-aggregated
+          ag_col = "ID", xgp = "cat", fgp = "cyl") 
 
 
-#pre-aggregated avg 
-avg <- aggregate(mtcars$mpg, list(mtcars$cyl), mean, na.rm = T)
-bar_graph(dat = avg, to_ag = F, type_of_ag = "avg", to_dodge = F,
-          sz = 4, ylav = 3, dgdwid = 0.75, 
-          ag_col = "mpg", xgp = "cyl") 
-#need to work this out so that if it is aggregated before w/ different names will still work,
-#and so that we don't need to type in the ag_col, xgp or fgp arguments when they're already there
-#maybe just make it so that you always aggregate in function, but could get annoying if 
-#need aggregations for other things and not just graph and you already have them loaded, why type if out again?
-
-
-
-
-# # #test
-# dat = mtcars
-# to_ag = T
-# type_of_ag = "percent"
-# to_dodge = T
-# sz = 4
-# ylav = 0.1
-# dgdwid = 0.75
-# ag_col = "ID"
-# xgp = "cat"
-# fgp = "cyl"
-# ag_col = "mpg"
-# xgp = "cyl"
-# fgp = "gear"
